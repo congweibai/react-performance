d611db623074a0a23d1c5348720f749160a1a312
Wyatt Bai <wyatt.bai@caremappr.com.au>
2024-11-25 21:29:54 +1030

    feat: web worker

--- exercises/05.calculations/02.problem.worker/src/cities/filter-cities.worker.ts
+++ exercises/05.calculations/02.problem.worker/src/cities/filter-cities.worker.ts
@@ -1,5 +1,7 @@
 // 🐨 you're gonna need this:
-// import * as Comlink from 'comlink'
+import * as Comlink from 'comlink'
+import cities from './us-cities.json'
+import { matchSorter } from 'match-sorter'
 // 🐨 move the stuff that's in index.ts into this file.
 
 // 🐨 create an object called "exposed" that has searchCities in it
@@ -7,3 +9,13 @@
 // 🦺 create and export a type called "Exposed" that is "typeof exposed"
 //   (you'll need this to get nice type safety in the main thread)
 // 💰 export type Exposed = typeof exposed
+
+const allCities = cities.map((city, index) => ({ ...city, id: String(index) }))
+
+export function searchCities(filter: string) {
+	return matchSorter(allCities, filter, { keys: ['name'] })
+}
+
+const exposed = { searchCities }
+Comlink.expose(exposed)
+export type Exposed = typeof exposed
--- exercises/05.calculations/02.problem.worker/src/cities/index.ts
+++ exercises/05.calculations/02.problem.worker/src/cities/index.ts
@@ -1,18 +1,12 @@
 // 🐨 move all this to a new file at ./filter-cities.worker.ts 👇
-import { matchSorter } from 'match-sorter'
-import cities from './us-cities.json'
 
-const allCities = cities.map((city, index) => ({ ...city, id: String(index) }))
-
-export function searchCities(filter: string) {
-	return matchSorter(allCities, filter, { keys: ['name'] })
-}
 // 🐨 move all that to a new file at ./filter-cities.worker.ts 👆
 
 // 🐨 🛑 finish the instructions in ./filter-cities.worker.ts before continuing here...
 
 // 🐨 you're going to want this:
-// import * as Comlink from 'comlink'
+import * as Comlink from 'comlink'
+import { type Exposed } from './filter-cities.worker'
 // 🐨 import the Exposed type from './fitler-cities.worker'
 
 // 🐨 create a new Worker object out of the ./filter-cities.worker.ts module
@@ -21,3 +15,16 @@ export function searchCities(filter: string) {
 // 🦺 you can use the Exposed type as the generic for some type safety
 
 // 🐨 export a new searchCities that calls the filterCities.searchCities API with a given input
+
+const worker = new Worker(
+	new URL('./filter-cities.worker.ts', import.meta.url),
+	{
+		type: 'module',
+	},
+)
+
+const filterCities = Comlink.wrap<Exposed>(worker)
+
+export async function searchCities(input: string) {
+	return filterCities.searchCities(input)
+}
